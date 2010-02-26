8d811bfe3da4ce0f7c7146b4165977d15424c975
Chris Aumann <me@chr4.org>
2010-02-26 16:20:40 +0100

    replaced system() with execlp()

--- serial.c
+++ serial.c
@@ -1,8 +1,6 @@
 /*
- * execv() instead of system()
- * 
- * consumption calculation kinda fails
- * 
+ * recover on failures
+ *
  * automate rrdtool database creation
  * create own folter for databases
  *
@@ -18,19 +16,20 @@
  * A5/5A proto initialization?
  *
  */
- 
-
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/signal.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 #include <unistd.h>
-//#include <sys/ioctl.h>
+
+// including ioctl.h conflicts with terminos.h
+// #include <sys/ioctl.h>
 
 #include <linux/termios.h>
 #include <linux/serial.h>
@@ -48,20 +47,44 @@ void kw1281_init(int);
 void kw1281_send_ack();
 void kw1281_send_block(unsigned char);
 void kw1281_recv_block(unsigned char);
-void inc_counter();
-
+int inc_counter();
+void exec_cmd(char *, float);
 
 int fd;
 int counter;			/* kw1281 protocol block counter */
 int ready = 0;
 
 float speed, rpm, temp1, temp2, oil_press, inj_time, load, voltage;
-float const_multiplier1 = 0.0089;
-float const_multiplier2 = 10000;
+float const_multiplier = 0.00000089;
 float const_inj_subtract = 0.1;
 float l_per_h;
 float l_per_100km;
 
+/* execute the rddtool command
+ * name = rdd file to write val to
+ * val = value
+ */
+void exec_cmd(char *name, float val)
+{
+
+	pid_t pid;
+	time_t t;
+	int status;
+	char cmd[256];
+
+	if ((pid = fork()) == 0) {
+		snprintf(cmd, sizeof(cmd), "%d:%.2f", (int)time(&t), val);
+
+		if (execlp("rrdtool", "touch", "update", name, cmd, NULL) == -1)
+			exit(-1);
+	}
+	if (pid < 0)
+		return;
+
+	waitpid(pid, &status, 0);
+
+	return;
+}
 
 /* manually set serial lines */
 static void _set_bit(int bit)
@@ -83,24 +106,28 @@ static void _set_bit(int bit)
 void kw1281_handle_error()
 {
 	/*
-	 recv() until 0x8a
-	 then send 0x75 (complement)
-	 reset counter = 1
-	 continue with block readings
+	   recv() until 0x8a
+	   then send 0x75 (complement)
+	   reset counter = 1
+	   continue with block readings
 
-	 or just exit -1 and start program in a loop
+	   or just exit -1 and start program in a loop
 	 */
+	printf("TRYING TO RECOVER WITH SENDING REQUEST\n");
+	kw1281_send_block(0x02);
 
 	exit(-1);
 }
 
 // increment the counter
-void inc_counter()
+int inc_counter()
 {
 	if (counter == 255)
 		counter = 1;
 	else
 		counter++;
+
+	return counter;
 }
 
 /* receive one byte and acknowledge it */
@@ -113,14 +140,14 @@ unsigned char kw1281_recv_byte_ack()
 	usleep(WRITE_DELAY);
 	write(fd, &d, 1);
 	read(fd, &d, 1);
-	if (0xff - c != d)
-	{
-		printf("kw1281_recv_byte_ack: echo error recv: 0x%02x (!= 0x%02x)\n", d, 0xff - c);
+	if (0xff - c != d) {
+		printf
+		    ("kw1281_recv_byte_ack: echo error recv: 0x%02x (!= 0x%02x)\n",
+		     d, 0xff - c);
 		kw1281_handle_error();
 	}
 	return c;
 }
-
 
 /* send one byte and wait for acknowledgement */
 void kw1281_send_byte_ack(unsigned char c)
@@ -130,20 +157,19 @@ void kw1281_send_byte_ack(unsigned char c)
 	usleep(WRITE_DELAY);
 	write(fd, &c, 1);
 	read(fd, &d, 1);
-	if (c != d)
-	{
-		printf("kw1281_send_byte_ack: echo error (0x%02x != 0x%02x)\n", c, d);
+	if (c != d) {
+		printf("kw1281_send_byte_ack: echo error (0x%02x != 0x%02x)\n",
+		       c, d);
 		kw1281_handle_error();
 	}
-	
+
 	read(fd, &d, 1);
-	if (0xff - c != d)
-	{
-		printf("kw1281_send_byte_ack: ack error (0x%02x != 0x%02x)\n", 0xff - c, d);
+	if (0xff - c != d) {
+		printf("kw1281_send_byte_ack: ack error (0x%02x != 0x%02x)\n",
+		       0xff - c, d);
 		kw1281_handle_error();
 	}
 }
-
 
 /* write 7O1 address byte at 5 baud and wait for sync/keyword bytes */
 void kw1281_init(int address)
@@ -160,7 +186,7 @@ void kw1281_init(int address)
 	usleep(INIT_DELAY);
 
 	_set_bit(0);		/* start bit */
-	usleep(INIT_DELAY);		/* 5 baud */
+	usleep(INIT_DELAY);	/* 5 baud */
 	p = 1;
 	for (i = 0; i < 7; i++) {	/* address bits, lsb first */
 		int bit = (address >> i) & 0x1;
@@ -182,23 +208,30 @@ void kw1281_init(int address)
 	ioctl(fd, FIONREAD, &in);
 	while (in--) {
 		read(fd, &c, 1);
-		//printf("ignore 0x%02x\n", c);
+#ifdef DEBUG
+		printf("ignore 0x%02x\n", c);
+#endif
 	}
 
 	read(fd, &c, 1);
-	//printf("read 0x%02x\n", c);
+#ifdef DEBUG
+	printf("read 0x%02x\n", c);
+#endif
 
 	read(fd, &c, 1);
-	//printf("read 0x%02x\n", c);
+#ifdef DEBUG
+	printf("read 0x%02x\n", c);
+#endif
 
 	c = kw1281_recv_byte_ack();
-	//printf("read 0x%02x (and sent ack)\n", c);
+#ifdef DEBUG
+	printf("read 0x%02x (and sent ack)\n", c);
+#endif
 
-	counter = 1;
-	
+	counter = 0;
+
 	return;
 }
-
 
 /* send an ACK block */
 void kw1281_send_ack()
@@ -210,8 +243,7 @@ void kw1281_send_ack()
 	/* block length */
 	kw1281_send_byte_ack(0x03);
 
-	kw1281_send_byte_ack(counter);
-	inc_counter();
+	kw1281_send_byte_ack(inc_counter());
 
 	/* ack command */
 	kw1281_send_byte_ack(0x09);
@@ -221,51 +253,46 @@ void kw1281_send_ack()
 	usleep(WRITE_DELAY);
 	write(fd, &c, 1);
 	read(fd, &c, 1);
-	if (c != 0x03)
-	{
+	if (c != 0x03) {
 		printf("echo error (0x03 != 0x%02x)\n", c);
 		kw1281_handle_error();
 	}
-	
+
 	return;
 }
-
 
 /* send group reading block */
 void kw1281_send_block(unsigned char n)
 {
 	unsigned char c;
-	
+
 #ifdef DEBUG
 	printf("send group reading block %d\n", counter);
 #endif
-	
+
 	/* block length */
 	kw1281_send_byte_ack(0x04);
 
 	// counter
-	kw1281_send_byte_ack(counter);
-	inc_counter();
-	
+	kw1281_send_byte_ack(inc_counter());
+
 	/*  type group reading */
 	kw1281_send_byte_ack(0x29);
-	
+
 	/* which group block */
 	kw1281_send_byte_ack(n);
-	
+
 	/* block end */
 	c = 0x03;
 	usleep(WRITE_DELAY);
 	write(fd, &c, 1);
 	read(fd, &c, 1);
-	if (c != 0x03)
-	{
+	if (c != 0x03) {
 		printf("echo error (0x03 != 0x%02x)\n", c);
 		kw1281_handle_error();
 	}
 	return;
 }
-
 
 /* receive a complete block */
 void kw1281_recv_block(unsigned char n)
@@ -273,7 +300,7 @@ void kw1281_recv_block(unsigned char n)
 	int i;
 	unsigned char c, l, t;
 	unsigned char buf[256];
-	
+
 	/* block length */
 	l = kw1281_recv_byte_ack();
 
@@ -289,20 +316,20 @@ void kw1281_recv_block(unsigned char n)
 		printf("0x%02x\t\t(counter)\n", c);
 		printf("     0x%02x\t(ack)\n", 0xff - c);
 		/*
-		while (1) {
-			c = kw1281_recv_byte_ack();
-			printf("0x%02x\t\t(data)\n", c);
-			printf("     0x%02x\t(ack)\n", 0xff - c);
-		}
-		*/
-#endif 
-		
+		   while (1) {
+		   c = kw1281_recv_byte_ack();
+		   printf("0x%02x\t\t(data)\n", c);
+		   printf("     0x%02x\t(ack)\n", 0xff - c);
+		   }
+		 */
+#endif
+
 		kw1281_handle_error();
 	}
 
 	t = kw1281_recv_byte_ack();
-	
-#ifdef DEBUG	
+
+#ifdef DEBUG
 	switch (t) {
 	case 0xf6:
 		printf("got ASCII block %d\n", counter);
@@ -326,114 +353,111 @@ void kw1281_recv_block(unsigned char n)
 	i = 0;
 	while (--l) {
 		c = kw1281_recv_byte_ack();
-		
+
 		buf[i++] = c;
-		
-#ifdef DEBUG		
+
+#ifdef DEBUG
 		printf("0x%02x ", c);
 #endif
-		
+
 	}
 	buf[i] = 0;
-	
+
 #ifdef DEBUG
 	if (t == 0xf6)
 		printf("= \"%s\"\n", buf);
 #endif
-	
+
 	if (t == 0xe7) {
-		
+
 		// look at field headers 0, 3, 6, 9
-		for (i = 0; i <= 9; i += 3)
-		{
-			switch (buf[i])
-			{
-				case 0x01: // rpm
-					if (i == 0)
-						rpm = 0.2 * buf[i+1] * buf[i+2];
-					break;
-					 
-				case 0x21: // load
-					if (i == 0)
-					{
-						if (buf[4] > 0)
-							load = (100*buf[i+2]) / buf[i+1];
-						else
-							load = 100;
-					}
-					break;
-					 
-				case 0x0f: // injection time
-					inj_time = 0.01 * buf[i+1] * buf[i+2];
-					break;
-					 
-				case 0x12: // absolute pressure
-					oil_press = 0.04 * buf[i+1] * buf[i+2];
-					break;
-					
-				case 0x05: // temperature
-					if (i == 6)
-						temp1 = buf[i+1] * (buf[i+2] - 100) * 0.1;
-					if (i == 9)
-						temp2 = buf[i+1] * (buf[i+2] - 100) * 0.1;
-					break;
-					
-				case 0x07: // speed
-					speed = 0.01 * buf[i+1] * buf[i+2];
-					break;
-					
-				case 0x15: // battery voltage
-					voltage = 0.001 * buf[i+1] * buf[i+2];
-					break;
-					
-				default:
+		for (i = 0; i <= 9; i += 3) {
+			switch (buf[i]) {
+			case 0x01:	// rpm
+				if (i == 0)
+					rpm = 0.2 * buf[i + 1] * buf[i + 2];
+				break;
+
+			case 0x21:	// load
+				if (i == 0) {
+					if (buf[4] > 0)
+						load =
+						    (100 * buf[i + 2]) / buf[i +
+									     1];
+					else
+						load = 100;
+				}
+				break;
+
+			case 0x0f:	// injection time
+				inj_time = 0.01 * buf[i + 1] * buf[i + 2];
+				break;
+
+			case 0x12:	// absolute pressure
+				oil_press = 0.04 * buf[i + 1] * buf[i + 2];
+				break;
+
+			case 0x05:	// temperature
+				if (i == 6)
+					temp1 =
+					    buf[i + 1] * (buf[i + 2] -
+							  100) * 0.1;
+				if (i == 9)
+					temp2 =
+					    buf[i + 1] * (buf[i + 2] -
+							  100) * 0.1;
+				break;
+
+			case 0x07:	// speed
+				speed = 0.01 * buf[i + 1] * buf[i + 2];
+				break;
+
+			case 0x15:	// battery voltage
+				voltage = 0.001 * buf[i + 1] * buf[i + 2];
+				break;
+
+			default:
 #ifdef DEBUG
-					printf("unknown value: 0x%02x: a = %d, b = %d\n", buf[i], buf[i+1], buf[i+2]);
+				printf
+				    ("unknown value: 0x%02x: a = %d, b = %d\n",
+				     buf[i], buf[i + 1], buf[i + 2]);
 #endif
-					break;
-			 }
-				
-				
-				
+				break;
+			}
+
 		}
-		
-	} 
+
+	}
 #ifdef DEBUG
 	else
 		printf("\n");
 #endif
-	
+
 	/* read block end */
 	read(fd, &c, 1);
-	if (c != 0x03)
-	{
+	if (c != 0x03) {
 		printf("block end error (0x03 != 0x%02x)\n", c);
 		kw1281_handle_error();
 	}
 
 	inc_counter();
 
-	
 	// set ready flag when receiving ack block
 	if (t == 0x09 && !ready) {
 		ready = 1;
 	}
-	
 	// set ready flag when sending 0x00 block after errors
 	if (t == 0x00 && !ready) {
 		ready = 1;
 	}
-	
-}
 
+}
 
 int main(int arc, char **argv)
 {
 	struct termios oldtio, newtio;
 	struct serial_struct st, ot;
-	time_t t;
-	char sysbuf[256];
-	
+
 	fd = open("/dev/ttyUSB0", O_SYNC | O_RDWR | O_NOCTTY);
 	if (fd < 0) {
 		printf("couldn't open device..\n");
@@ -457,7 +481,7 @@ int main(int arc, char **argv)
 	}
 
 	newtio.c_cflag = B38400 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR; // ICRNL provokes bogus replys after block 12
+	newtio.c_iflag = IGNPAR;	// ICRNL provokes bogus replys after block 12
 	newtio.c_oflag = 0;
 	newtio.c_cc[VMIN] = 1;
 	newtio.c_cc[VTIME] = 0;
@@ -481,24 +505,17 @@ int main(int arc, char **argv)
 		kw1281_send_block(0x02);
 		kw1281_recv_block(0x02);
 		// we have rpm, inj_time, oil_press, load
-		
+
 		if (inj_time > const_inj_subtract)
-			l_per_h = 60 * 4 * const_multiplier1 * const_multiplier1 * rpm * (inj_time - const_inj_subtract);
+			l_per_h =
+			    60 * 4 * const_multiplier * rpm * (inj_time -
+							       const_inj_subtract);
 		else
 			l_per_h = 0;
 
-		
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update rpm.rrd %d:%.0f",
-				 (int) time(&t), rpm);
-		system(sysbuf);
+		exec_cmd("rpm.rdd", rpm);
+		exec_cmd("con_h.rrd", l_per_h);
 
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update con_h.rrd %d:%.2f",
-				 (int) time(&t), l_per_h);
-		system(sysbuf);		
-		
-		
 		kw1281_send_block(0x05);
 		kw1281_recv_block(0x05);
 		// we have speed
@@ -507,39 +524,18 @@ int main(int arc, char **argv)
 			l_per_100km = l_per_h / speed;
 		else
 			l_per_100km = -1;
-		
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update speed.rrd %d:%.1f",
-				 (int) time(&t), speed);
-		system(sysbuf);
-		
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update con_km.rrd %d:%.2f",
-				 (int) time(&t), l_per_100km);
-		system(sysbuf);		
-		
-		// function with execv("speed", 
-		
+
+		exec_cmd("speed.rrd", speed);
+		exec_cmd("con_km.rrd", l_per_100km);
+
 		kw1281_send_block(0x04);
 		kw1281_recv_block(0x04);
 		// we have both temperatures and voltage
-		
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update temp1.rrd %d:%.1f",
-				 (int) time(&t), temp1);
-		system(sysbuf);
-		
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update temp2.rrd %d:%.1f",
-				 (int) time(&t), temp2);
-		system(sysbuf);
-		
-		snprintf(sysbuf, sizeof(sysbuf),
-				 "rrdtool update voltage.rrd %d:%.2f",
-				 (int) time(&t), voltage);
-		system(sysbuf);
-		
-		
+
+		exec_cmd("temp1.rrd", temp1);
+		exec_cmd("temp2.rrd", temp2);
+		exec_cmd("voltage.rrd", voltage);
+
 		printf("----------------------------------------\n");
 		printf("l/h\t\t%.2f\n", l_per_h);
 		printf("l/100km\t\t%.2f\n", l_per_100km);
