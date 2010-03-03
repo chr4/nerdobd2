3329c44713eec88bf34671df0908f8ece44f7261
Chris Aumann <me@chr4.org>
2010-03-03 21:29:18 +0100

    replaced system() with execvp()

--- rrdtool.c
+++ rrdtool.c
@@ -1,45 +1,93 @@
 #include "serial.h"
 
-char    rrdstyle[1024] = "--slope-mode --height=230 --width=490 --full-size-mode --rigid "
-                         // 30sec grid line, major grid 1 min, lables 1 min, %M:%M
-                         "--x-grid SECOND:30:MINUTE:1:MINUTE:1:0:%R "
-                         "--color=SHADEB#222222 --color=SHADEA#222222 "
-                         "--color=BACK#222222 --color=FRAME#222222 "
-                         "--color=GRID#aaaaaa --color=MGRID#aaaaaa "
-                         "--color=CANVAS#eeeeee --color=AXIS#aaaaaa "
-                         "--color=FONT#aaaaaa --color=ARROW#aaaaaa";
+
+/* array with strings for rrdtool global options */
+char *rrdstyle[256] =
+{ 
+    "--slope-mode", "--height", "230", "--width", "490", "--full-size-mode", "--rigid",
+    "--x-grid", "SECOND:30:MINUTE:1:MINUTE:1:0:%R",
+    "--color", "SHADEB#222222", "--color", "SHADEA#222222",
+    "--color", "BACK#222222", "--color", "FRAME#222222",
+    "--color", "GRID#aaaaaa", "--color", "MGRID#aaaaaa",
+    "--color", "CANVAS#eeeeee", "--color", "AXIS#aaaaaa",
+    "--color", "FONT#aaaaaa", "--color", "ARROW#aaaaaa",
+    NULL    // array needs to be terminated with NULL
+};
 
 
 void *
 rrdtool_update_consumption ()
 {
-    char    cmd[1024];
+    char    starttime[256];
+    char    endtime[256];    
     time_t  t;
     
     if (con_km < 0)
-        snprintf (cmd, sizeof (cmd), "rrdtool update consumption.rrd %d::%.2f &", 
+        snprintf (starttime, sizeof (starttime), "%d::%.2f", 
                   (int) time (&t), con_h);
     else
-        snprintf (cmd, sizeof (cmd), "rrdtool update consumption.rrd %d:%.2f:%.2f &", 
+        snprintf (starttime, sizeof (starttime), "%d:%.2f:%.2f", 
                   (int) time (&t), con_km, con_h);
     
-    if (system(cmd) == -1)
-        perror("system() ");
+    if (fork() == 0)
+    {
+        execlp("rrdtool", "rrdtool",
+               "update", "consumption.rrd", starttime,
+               NULL);
+
+        exit(0);
+    }
+    
+    // we want to graph the last 5 mins
+    snprintf(starttime, sizeof(starttime), "%d",
+             (int) time (&t) - 300); // now - 300s (5min)
+    
+    snprintf(endtime, sizeof(endtime), "%d",
+             (int) time (&t));
+    
+    
+    if (fork() == 0)
+    {
+        int i = 0, n = 0;
+        char combined[256][256];
         
-    
-    
-    snprintf (cmd, sizeof (cmd), 
-              "rrdtool graph consumption.png --start %d --end %d %s "
-              "--upper-limit=19.5 --lower-limit=0 --y-grid 3.25:1 "
-              "DEF:con_km=consumption.rrd:km:AVERAGE "
-              "DEF:con_h=consumption.rrd:h:AVERAGE "
-              "AREA:con_km#f00000:l/100km "
-              "LINE2:con_h#00f000:l/h "
-              "&> /dev/null &", 
-              (int) time (&t) - 300, (int) time (&t), rrdstyle );
-    
-    if (system(cmd) == -1)
-        perror("system() "); 
+        char *args[256] =
+        {             
+            "rrdtool", "graph", "consumption.png",
+            "--start", starttime, "--end", endtime,
+            "--upper-limit", "19.5", "--lower-limit", "0", "--y-grid", "3.25:1",
+            "DEF:con_km=consumption.rrd:km:AVERAGE", 
+            "DEF:con_h=consumption.rrd:h:AVERAGE", 
+            "AREA:con_km#f00000:l/100km", 
+            "LINE2:con_h#00f000:l/h",
+            NULL    // array needs to be terminated with NULL
+        };
+        
+        
+        
+        // append args to combined
+        while (args[i] != NULL) 
+        {
+            strncpy(combined[i], args[i], sizeof(combined[i]));
+            i++;
+        }
+        
+        // append rrdstyle to combined
+        while (rrdstyle[n] != NULL) 
+        {
+            strncpy(combined[i], rrdstyle[n], sizeof(combined[i]));
+            n++;
+            i++;
+        }
+        
+        // we need an array of pointers for execvp()
+        for (n = 0; n < i; n++)
+            args[n] = combined[n];
+        
+        
+        execvp(args[0], args);
+        exit(0);
+    }
     
     return (void *) NULL;
 }
@@ -47,24 +95,69 @@ rrdtool_update_consumption ()
 void *
 rrdtool_update_speed ()
 {
-    char    cmd[1024];
+    char    starttime[256];
+    char    endtime[256];
     time_t  t;
     
-    snprintf (cmd, sizeof (cmd), "rrdtool update speed.rrd %d:%.1f &", 
+    snprintf (starttime, sizeof (starttime), "%d:%.1f", 
               (int) time (&t), speed);
     
-    if (system(cmd) == -1)
-        perror("system() ");    
-    snprintf (cmd, sizeof (cmd), 
-              "rrdtool graph speed.png --start %d --end %d %s "
-              "--upper-limit=135 --lower-limit=0 --y-grid 7.5:2 "
-              "DEF:myspeed=speed.rrd:speed:AVERAGE "
-              "AREA:myspeed#0000f0:speed "
-              "&> /dev/null &",
-              (int) time (&t) - 300, (int) time (&t), rrdstyle );
+    if (fork() == 0)
+    {
+        execlp("rrdtool", "rrdtool",
+               "update", "speed.rrd", starttime,
+               NULL);
+        
+        exit(0);
+    }
+
+    // we want to graph the last 5 mins
+    snprintf(starttime, sizeof(starttime), "%d",
+             (int) time (&t) - 300); // now - 300s (5min)
     
-    if (system(cmd) == -1)
-        perror("system() ");
+    snprintf(endtime, sizeof(endtime), "%d",
+             (int) time (&t));
+    
+    
+    if (fork() == 0)
+    {
+        int i = 0, n = 0;
+        char combined[256][256];
+
+        char *args[256] =
+        { 
+            "rrdtool", "graph", "speed.png",
+            "--start", starttime, "--end", endtime,
+            "--upper-limit", "135", "--lower-limit", "0", "--y-grid", "7.5:2",
+            "DEF:myspeed=speed.rrd:speed:AVERAGE",
+            "AREA:myspeed#0000f0:speed", 
+            NULL    // array needs to be terminated with NULL
+        };
+        
+        
+        // append args to combined
+        while (args[i] != NULL) 
+        {
+            strncpy(combined[i], args[i], sizeof(combined[i]));
+            i++;
+        }
+        
+        // append rrdstyle to combined
+        while (rrdstyle[n] != NULL) 
+        {
+            strncpy(combined[i], rrdstyle[n], sizeof(combined[i]));
+            n++;
+            i++;
+        }
+        
+        // we need an array of pointers for execvp()
+        for (n = 0; n < i; n++)
+            args[n] = combined[n];
+
+        
+        execvp(args[0], args);
+        exit(0);
+    }
     
     return (void *) NULL;
 }
