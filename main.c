c7404a112e12ed9ca2b260fcfd59248b837af8db
kris kechagia <kk@rndsec.net>
2011-04-16 00:34:26 +0200

    removed everything but absolutely bare code

--- main.c
+++ main.c
@@ -1,95 +1,9 @@
 #include "serial.h"
 
-/*
- * start like this:
- * # while (true); do ./serial; done
- *
- * thanks: Florian Schäffer     http://www.blafusel.de/obd/obd2_kw1281.html
- *         Philipp aka pH5      http://www.blafusel.de/phpbb/viewtopic.php?t=223
- *         freediag             http://freediag.org/
- *
- *
- * TODO:
- *
- * fix liters calculation with measuring time between calls
- *
- * fix average calculations:
- *   - measure time
- *   - better handling of 0km / 0l/100km handling
- *
- * set baudrate, multiplicator value and other things via config file
- * set baudrate from argv
- *
- */
-
-void	reset_values(void);
 void	cleanup (int);
-void    refresh_tank_content(void);
-
-// pid of ajax server
-pid_t   pid;
 
 // flag for cleanup function
 char	cleaning_up = 0;
-
-
-void
-reset_values(void)
-{
-    return;
-}
-
-// refreshes tank content
-void
-refresh_tank_content(void)
-{
-    int     trys;
-
-#ifdef SERIAL_ATTACHED
-    int     ret;
-#endif
-
-    // try up to TANK_CONT_MAX_TRYS times
-    for (trys = 0; trys < TANK_CONT_MAX_TRYS; trys++)
-    { 
-        
-#ifdef SERIAL_ATTACHED        
-        // ECU: 0x01, INSTR: 0x17
-        // send 5baud address, read sync byte + key word
-        ret = kw1281_init (0x17);
-    
-        // soft error, e.g. communication error
-        if (ret == -1) {
-            ajax_log("init (tank content) failed, retrying...\n");
-            continue;
-        }
-    
-        // hard error (e.g. serial cable unplugged)
-        else if (ret == -2)
-        {
-            ajax_log("serial port error\n");
-            cleanup(0);
-        }
-    
-#endif
-    
-        if (kw1281_get_tank_cont() == -1)
-        {
-            ajax_log("error getting tank content, retrying...\n");
-            continue;
-        }
-        // on success, break
-        else
-        {
-            ajax_log("tank content updated.\n");
-            break;
-        }
-    }
-    
-    // reset flag
-    tank_request = 0;
-}
-
 
 void
 cleanup (int signo)
@@ -102,14 +16,8 @@ cleanup (int signo)
 	
 	printf("\ncleaning up:\n");
 	
-#ifdef SERIAL_ATTACHED		
 	printf("closing serial port...\n");
 	kw1281_close();
-#endif
-	
-	// send kill to ajax server 
-	printf("sending SIGTERM to ajax server...\n");
-	kill(pid, SIGTERM);
 	
 	printf("exiting.\n");
 	exit(0);
@@ -121,31 +29,11 @@ main (int arc, char **argv)
 {
     int     ret;
     
-    
-#ifdef SERIAL_ATTACHED
     // kw1281_open() somehow has to be started
     // before any fork() open()
     if (kw1281_open (DEVICE) == -1)
         return -1;
-#endif
     
-    // initialize values (if possible, load from file)
-    if (init_db() == -1)
-        return -1;
-    
-    // create databases, unless they exist
-    rrdtool_create_consumption ();
-    rrdtool_create_speed ();
-
-    
-    // create ajax socket in child process for handling http connections
-    if ( (pid = fork()) > 0)
-    {
-        ajax_socket(PORT);
-        exit(0);
-    }
-    
-	
     // add signal handler for cleanup function
     signal(SIGINT, cleanup);
     signal(SIGTERM, cleanup);	
@@ -169,15 +57,9 @@ main (int arc, char **argv)
         printf("sorry, need to be root for realtime priority. continuing with normal priority.\n");
 #endif
     
-    
-    // getting tank content once before entering main loop
-    // refresh_tank_content();
-    
-    
     for ( ; ; )
     {
         
-#ifdef SERIAL_ATTACHED
         printf ("init\n");                
         
         // ECU: 0x01, INSTR: 0x17
@@ -187,18 +69,16 @@ main (int arc, char **argv)
         // soft error, e.g. communication error
         if (ret == -1)
         {
-            ajax_log("init failed, retrying...\n");
-            reset_values();
+            printf("init failed, retrying...\n");
             continue;
         }
         
         // hard error (e.g. serial cable unplugged)
         else if (ret == -2)
         {
-            ajax_log("serial port error\n");
+            printf("serial port error\n");
             cleanup(0);
         }
-#endif
 
         
         ret = kw1281_mainloop();
@@ -206,18 +86,10 @@ main (int arc, char **argv)
         // on errors, restart
         if (ret == -1)
         {
-            ajax_log("errors. restarting...\n");
-            reset_values();
+            printf("errors. restarting...\n");
             continue;
         }
         
-        // if mainloop() exited due to tank request
-        else if (ret == TANK_REQUEST)
-        {
-            // grey out values
-            reset_values();
-            refresh_tank_content();
-        }
     }
 
     // should never be reached
--- serial.h
+++ serial.h
@@ -18,26 +18,12 @@
 #include <errno.h>
 #include <signal.h>
 
-// sqlite database
-#include <sqlite3.h>
-
 // for priority settings
 #include <sched.h>
 
-// shm includes
-#include <sys/ipc.h>
-#include <sys/shm.h>
-
-// include rrd stuff (librrd-dev)
-#include <rrd.h>
-
-
-//#define SERIAL_ATTACHED   // define if serial cable is attached (for debugging, comment out)
 #define HIGH_PRIORITY       // whether to run program with high priority (for more reliable serial communication)
 
 #define DEBUG_SERIAL
-// #define DEBUG_AJAX
-// #define DEBUG_SQLITE
 
 #define DEVICE          "/dev/ttyUSB0"
 #define BAUDRATE        10400   // for my seat arosa, vw polo needs 9600
@@ -54,53 +40,7 @@
 #define MULTIPLIER      0.00000089  // for my 1.0l seat arosa 2004
 #define INJ_SUBTRACT    0.1
 
-// the port where to local ajax server listens
-#define PORT            8080
-
-#define TANK_CONT_MAX_TRYS 5    // try to get tank content up to 5 times
-#define TANK_REQUEST    3       // return value
-
-#define SPEED_GRAPH     "speed.png"
-#define CON_GRAPH       "consumption.png"
-
-// the database file (sqlite3) and file handler
-#define DB_FILE         "database.sqlite3"
-sqlite3 *db;
-
-void    rrdtool_create_speed (void);
-void    rrdtool_create_consumption (void);
-void    rrdtool_update_speed (void);
-void    rrdtool_update_consumption (void);
-
 int     kw1281_open (char *device);
 int     kw1281_close(void);
 int     kw1281_init (int);
-int     kw1281_get_tank_cont(void);
 int     kw1281_mainloop (void);
-
-void    ajax_log(char *s);
-void    ajax_socket(int);
-
-// sqlite functions
-int     exec_query(char *);
-int     create_table(char *);
-int     insert_value(char *, float);
-float   get_value(char *);
-float   get_row(char *, char *);
-float   get_average(char*, char *, int);
-int     calc_consumption(void);
-int     init_db(void);
-void    close_db(void);
-
-
-// debuging messages go here
-char    *debug; 
-
-// make server socket global so we can close/shutdown it on exit
-int     srv;
-
-// flag for tank request
-char    tank_request;
-
-int	speed_graph_timespan;
-int	consumption_graph_timespan;
