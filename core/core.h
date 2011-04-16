e6148a3589c4b5a3808df1e2b1d99e60224d1e81
kris kechagia <kk@rndsec.net>
2011-04-16 17:58:51 +0200

    removed unused includes

--- core/core.h
+++ core/core.h
@@ -1,20 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/select.h>
-#include <sys/stat.h>
-#include <sys/time.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <termios.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/serial.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <errno.h>
 #include <signal.h>
 
