6252d88391802ddb7ef888012bb0cea31d0e7192
Chris Aumann <me@chr4.org>
2011-06-17 23:49:29 +0200

    created kw1281.h and tidyed up other header files

--- core.h
+++ core.h
@@ -1,29 +1,21 @@
 // TODO: check whether terminal/socket includes can be in the file that needs them only
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/serial.h>
 #include <errno.h>
 #include <signal.h>
-#include <sys/time.h>
 
 // include common defines
 #include "common.h"
 #include "config.h"
+
+// serial protocol
+#include "kw1281.h"
 
 // we need sqlite3 access
 #include "sqlite.h"
 
 // the http server
 #include "httpd.h"
-
-#define SERIAL_HARD_ERROR   -2
-#define SERIAL_SOFT_ERROR   -1
-
 
 // the engine data struct
 typedef struct engine_data
@@ -51,15 +43,5 @@ typedef struct other_data
     
 } other_data;
 
-
-// flags if this is the first run
-char consumption_first_run;
-char speed_first_run;
-
-
-int  kw1281_open (char *device);
-int  kw1281_close(void);
-int  kw1281_init (int);
-int  kw1281_mainloop (void);
 
 void handle_data(char *, float, float);
--- sqlite.h
+++ sqlite.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
