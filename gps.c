53432a3439b98a55778e899c6f94edbff6062f8d
Chris Aumann <me@chr4.org>
2012-09-06 15:24:31 +0200

    adds gps_stream() and gps_waiting()

--- gps.c
+++ gps.c
@@ -6,14 +6,20 @@
 static struct gps_data_t gpsdata;
 char gps_available = 0;
 
+
 int
-get_gps_data(struct gps_fix_t *g)
-{
+get_gps_data(struct gps_fix_t *g) {
+
     if (!gps_available)
         return -1;
 
-    if (gps_read(&gpsdata) <= 0)
-    {   
+    if (!gps_waiting(&gpsdata, 10)) {
+        fprintf(stderr, "no gps data available (timeout)\n");
+        return -1;
+    }
+
+    errno = 0;
+    if (gps_read(&gpsdata) == -1) {
         perror("gps_read()");
         return -1;
     }
@@ -28,21 +34,32 @@ get_gps_data(struct gps_fix_t *g)
 }
 
 void
-gps_stop(void)
-{
-    if (gps_available)
-        gps_close( (struct gps_data_t *) &gps_data);
+gps_stop(void) {
+
+    if (!gps_available)
+        return;
+
+    gps_stream( (struct gps_data_t *) &gpsdata, WATCH_DISABLE, NULL);
+    gps_close( (struct gps_data_t *) &gpsdata);
 }
 
+
 int
-gps_start(void)
-{
-    // open the stream to gpsd
-    // if (gps_open(GPSD_SHARED_MEMORY, NULL, &gpsdata) != 0) {
-    if (gps_open("localhost", "2947", &gpsdata) != 0) {
+gps_start(void) {
+
+    /* open the stream to gpsd
+     * shared memory segment kinda isn't available anymore?
+     *
+     * if (gps_open(GPSD_SHARED_MEMORY, NULL, &gpsdata) != 0) {
+     */
+    if (gps_open("localhost", DEFAULT_GPSD_PORT, &gpsdata) != 0) {
         printf("couldn't connect to gpsd: %s\n", gps_errstr(errno));
         return -1;
     }
+
+    // watching is not available (and not needed) when using shared memory segment
+    if (gps_stream( (struct gps_data_t *) &gpsdata, WATCH_ENABLE, NULL) == -1)
+        perror("gps_stream()");
 
     gps_available = 1;
     return 0;
